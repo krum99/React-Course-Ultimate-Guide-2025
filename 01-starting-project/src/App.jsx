b51075740afcd8fcba1c65c8b37a78f44b32eadb
krum99 <79230210+krum99@users.noreply.github.com>
2025-08-27 17:12:37 +0300

    Add new section.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 import { CORE_CONCEPTS } from './data';
 
@@ -20,6 +21,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
