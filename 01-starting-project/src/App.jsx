04fb5e8946e4db74f2b111fd7bb5f4a91c021eaa
krum99 <79230210+krum99@users.noreply.github.com>
2025-08-29 18:07:11 +0300

    Empty state at init.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -6,7 +6,7 @@ import TabButton from './components/TabButton';
 import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   let tabContent = 'Click a button';
   function handleSelect(selectedButton) {
@@ -40,13 +40,17 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : (
+            <p>Please, select a topic.</p>
+          )}
         </section>
       </main>
     </div>
