ae311d2882a380781096ac88cbe05809d3c44c30
krum99 <79230210+krum99@users.noreply.github.com>
2025-09-03 16:51:16 +0300

    update Player component : track if a player is active

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(name);
   function handleChange(event) {
     console.log(event);
@@ -22,7 +22,7 @@ export default function Player({ name, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {playerNameContainer}
         <span className="player-symbol">{symbol}</span>
