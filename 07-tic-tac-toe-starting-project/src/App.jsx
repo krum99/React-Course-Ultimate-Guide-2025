c548acd174475acb711e44f4c40fdfc195405bf7
krum99 <79230210+krum99@users.noreply.github.com>
2025-09-03 16:51:02 +0300

    update App component : track if a player is active

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelect() {
+    setActivePlayer((curPlayer) => (curPlayer === 'O' ? 'X' : 'O'));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
+        <ol id="players" className="highlight-player">
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelect} />
       </div>
       LOG
     </main>
