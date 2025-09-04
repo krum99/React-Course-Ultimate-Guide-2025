99f7c0eff9fc4801333e93852e94988a95986f37
krum99 <79230210+krum99@users.noreply.github.com>
2025-09-04 20:36:03 +0300

    keep players name list dynamically

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol, isActive }) {
+export default function Player({ name, symbol, isActive, onChangeName }) {
   const [playerName, setPlayerName] = useState(name);
   function handleChange(event) {
     console.log(event);
@@ -10,6 +10,10 @@ export default function Player({ name, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
   function handleEditOnClick() {
     setIsEditing((isEditing) => !isEditing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   let playerNameContainer = <span className="player-name">{playerName}</span>;
