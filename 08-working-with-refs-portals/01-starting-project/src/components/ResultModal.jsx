aab638646967bcba66deb4ffe5caa7079f9f2546
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-17 18:28:46 +0200

    update ResultModal

--- 08-working-with-refs-portals/01-starting-project/src/components/ResultModal.jsx
+++ 08-working-with-refs-portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
 export default function ResultModal({ result, targetTime }) {
   return (
-    <dialog className="result-modal">
+    <dialog className="result-modal" open>
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
