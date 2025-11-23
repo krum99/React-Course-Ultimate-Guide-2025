ea50dfc27f22151c98b455f073800729b6aaf406
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-23 20:42:00 +0200

    useEffect cleanup function

--- 11-handling-side-effects-useEffect-hook/01-starting-project/src/App.jsx
+++ 11-handling-side-effects-useEffect-hook/01-starting-project/src/App.jsx
@@ -73,10 +73,12 @@ function App() {
   return (
     <>
       <Modal open={modalIsOpen}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
+        {modalIsOpen && (
+          <DeleteConfirmation
+            onCancel={handleStopRemovePlace}
+            onConfirm={handleRemovePlace}
+          />
+        )}
       </Modal>
 
       <header>
