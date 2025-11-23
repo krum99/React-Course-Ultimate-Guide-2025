651d0b17ef115c2a2e6a15c155168f429b5a2047
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-23 21:04:45 +0200

    use Callback hook : do not recreate function that is a dependency

--- 11-handling-side-effects-useEffect-hook/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 11-handling-side-effects-useEffect-hook/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -8,7 +8,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
     return () => {
       clearTimeout(timer);
     };
-  }, []);
+  }, [onConfirm]);
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
