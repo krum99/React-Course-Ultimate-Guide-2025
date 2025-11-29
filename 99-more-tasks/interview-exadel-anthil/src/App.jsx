2433276e965c9047d31d87ea561a3fe541f63246
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-29 19:43:03 +0200

    App: update data model

--- 99-more-tasks/interview-exadel-anthil/src/App.jsx
+++ 99-more-tasks/interview-exadel-anthil/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import AddButton from './components/AddButton';
+import DataVisualizer from './components/DataVisualizer';
 
 function App() {
   const [data, setData] = useState([]);
 
   function addHandler(new_data) {
-    setData((prev) => [new_data, ...prev]);
+    setData((prev) => [
+      { data: new_data, checked: false, id: uuidv4() },
+      ...prev,
+    ]);
   }
   console.log(data);
 
   return (
     <>
       <AddButton onCLickHandler={addHandler} />
+      <DataVisualizer data={data} />
     </>
   );
 }
