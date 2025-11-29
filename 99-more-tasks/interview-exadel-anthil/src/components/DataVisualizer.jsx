ec06aa36ff518938f0533b449031d05d9cf32498
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-29 20:05:33 +0200

    update DataVisualization

--- 99-more-tasks/interview-exadel-anthil/src/components/DataVisualizer.jsx
+++ 99-more-tasks/interview-exadel-anthil/src/components/DataVisualizer.jsx
@@ -1,15 +1,23 @@
-// import { v4 as uuidv4 } from 'uuid';
-
-export default function DataVisualizer({ data }) {
-  // function handleCheck(){
-
-  // }
-
+export default function DataVisualizer({ data, onClickLi }) {
   return (
     <ul>
       {data.map((obj) => (
-        <li key={obj.id} style={{ border: '1px solid black' }}>
-          <input type="checkbox" id={obj.id} checked={obj.checked} />
+        <li
+          key={obj.id}
+          style={{
+            border: '1px solid black',
+            padding: '10px',
+            margin: '5px 0',
+            cursor: 'pointer',
+          }}
+          onClick={() => onClickLi(obj.id)}
+        >
+          <input
+            type="checkbox"
+            checked={obj.checked || false}
+            readOnly
+            style={{ marginRight: '10px' }}
+          />
           {obj.data}
         </li>
       ))}
