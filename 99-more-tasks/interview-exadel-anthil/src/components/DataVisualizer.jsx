2c5d738daae929d84c0f7f8869ba13997e6b58d9
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-29 19:43:24 +0200

    DataVisualizer : add checkbox

--- 99-more-tasks/interview-exadel-anthil/src/components/DataVisualizer.jsx
+++ 99-more-tasks/interview-exadel-anthil/src/components/DataVisualizer.jsx
@@ -1,10 +1,17 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 export default function DataVisualizer({ data }) {
+  // function handleCheck(){
+
+  // }
+
   return (
     <ul>
-      {data.map((value) => (
-        <li key={uuidv4()}>{value}</li>
+      {data.map((obj) => (
+        <li key={obj.id} style={{ border: '1px solid black' }}>
+          <input type="checkbox" id={obj.id} checked={obj.checked} />
+          {obj.data}
+        </li>
       ))}
     </ul>
   );
